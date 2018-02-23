52119890f772d9dbc84107b26ef382698c29f2ff
Shurong Wei <Shu@Shus-MacBook-Air.local>
2018-02-23 14:56:07 +0800

    v-model sync on save in w-input

--- src/components/WInput.vue
+++ src/components/WInput.vue
@@ -5,12 +5,11 @@
 </style>
 <template>
 <div class="w-input" :class="">
-    <el-input v-if="isEditable"
+    <el-input v-if="isEditing"
         v-bind="$props"
         v-model="currentValue"
         @keyup.native.enter="save"
         @keyup.native.esc="cancel"
-        @blur="cancel"
     />
     <p v-else
         @click="onClick"
@@ -18,9 +17,14 @@
         type="text">
         {{currentValue || placeholder}}
     </p>
-    <w-button
-    >保存</w-button>
-    <el-button plain>取消</el-button>
+    <div v-if="isEditing">
+        <w-button
+            :handler="() => save()"
+        >保存</w-button>
+        <el-button 
+            @click="cancel"
+        plain>取消</el-button>
+    </div>
 </div>
 </template>
 
@@ -32,11 +36,13 @@ export default {
     name: "WInput",
     watch: {
         value(val) {
+            console.log("watch");
             this.currentValue = val;
         }
     },
     mounted () {
         this.$nextTick(() => {
+            console.log("nextTick");
             this.currentValue = this.value;
         });
     },
@@ -46,12 +52,16 @@ export default {
                 color: "black",
                 cursor: "inherit"
             },
-            isEditable: false,
+            isEditing: false,
             currentValue: "",
             events: {
                 ...this.$listeners
             }
         };
+    },
+    model: {
+        prop: "value",
+        event: "save"
     },
     props: {
         ...Input.props,
@@ -69,27 +79,28 @@ export default {
         onClick (event) {
             if (!this.clickable) return;
             event.stopPropagation();
-            this.isEditable = true;
+            this.isEditing = true;
         },
         cancel () {
-            this.isEditable = false;
+            console.log("cancel");
+            this.isEditing = false;
             this.currentValue = this.value;
             this.$emit("cancel");
             this.$emit("finish");
         },
         edit () {
-            this.isEditable = true;
+            this.isEditing = true;
             this.disabled = false;
             this.$emit("edit");
         },
         async save() {
-            this.isEditable = false;
+            this.isEditing = false;
             if (!this.name || !this.api) return;
             const data = {
                 [this.name]: this.currentValue
             };
             await this.$request({ url: this.api, data, method: "put"});
-            this.$emit("save");
+            this.$emit("save", this.currentValue);
             this.$emit("finish");
         }
     }
